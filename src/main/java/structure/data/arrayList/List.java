453548a31932d2ea196a9024e0b26b2dc3dd44a9
sylim <sammys27@naver.com>
2024-07-26 22:15:05 +0900

    디렉토리 구조 변경

--- src/main/java/structure/data/arrayList/List.java
+++ src/main/java/structure/data/arrayList/List.java
@@ -1,12 +1,20 @@
 package structure.data.arrayList;
 
 interface List<E> {
+
     boolean add(E element);
+
     boolean remove(int index);
+
     boolean remove(E e);
+
     E get(int index);
+
     E set(int index, E element);
+
     int indexOf(Object element);
+
     int size();
+
     boolean contains(Object o);
 }
