15f7cebd3fd365a6fbed61ba02ce07a5df829711
sylim <sammys27@naver.com>
2024-07-26 21:16:54 +0900

    List 인터페이스 추가

---
+++ src/arrayList/List.java
@@ -0,0 +1,12 @@
+package arrayList;
+
+interface List<E> {
+    boolean add(E element);
+    boolean remove(int index);
+    boolean remove(E e);
+    E get(int index);
+    E set(int index, E element);
+    int indexOf(Object element);
+    int size();
+    boolean contains(Object o);
+}
